792f053c8decc79a77318e1172f440db63b2091b
weerapata1 <seal_recon_00@hotmail.com>
2020-08-25 03:12:15 +0700

    RemoveFileProject

--- src/main/java/com/example/server/Repository/ClientTypeRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.server.Repository;
-
-import com.example.server.Entity.ClientType;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
-@RepositoryRestResource
-public interface ClientTypeRepository extends JpaRepository <ClientType, Long> {
-}
