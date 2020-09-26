b068358f1f8ee9a97e22f2c85a6189a12ba5bd65
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2020-09-26 23:52:52 +0700

    Update ClientTypeRepository.java

--- server/src/main/java/com/example/server/Repository/ClientTypeRepository.java
+++ server/src/main/java/com/example/server/Repository/ClientTypeRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ClientTypeRepository extends JpaRepository <ClientType, Long> {
+    ClientType findByNameType(String nameType);
 }
