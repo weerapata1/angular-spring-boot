c1082a620ad4831b919dd85a893fed7eae0210bf
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2020-09-26 23:51:50 +0700

    Update Client.java

--- server/src/main/java/com/example/server/Entity/Client.java
+++ server/src/main/java/com/example/server/Entity/Client.java
@@ -4,7 +4,10 @@ import lombok.*;
 
 import javax.annotation.Generated;
 import javax.persistence.*;
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,20 +20,24 @@ import java.util.Optional;
 @ToString
 public class Client {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY, generator = "Client_seq")
     @SequenceGenerator(name = "Client_seq",sequenceName = "Client_seq")
-    @Column(name = "clientId",unique = true ,nullable = false)
+    @Column(name = "id",unique = true ,nullable = false)
+    @NotNull(message = "\t--> clientId is Null <--\t")
     private Long id;
 
     @Column(unique = false)
+    @Size(max = 50, message = "\t--> firstNameClient over length <--\t" )
     @NotNull(message = "\t--> firstNameClient is Null <--\t")
     private String FName;
 
     @Column(unique = false)
+    @Size(max = 50, message = "\t--> lastNameClient over length <--\t" )
     @NotNull(message = "\t--> lastNameClient is Null <--\t")
     private String LName;
 
     @Column(unique = false)
+    @Pattern(regexp = "\\d{13}")
     @NotNull(message = "\t--> citizenIDClient is Null <--\t")
     private String citizenID;
 
@@ -41,10 +48,11 @@ public class Client {
     @JoinColumn(name = "typeID",insertable = true)
     private ClientType clientType;
 
-    public Client(String FName, String LName, String citizenID){
+    public Client(String FName, String LName, String citizenID,ClientType ClientType){
         this.FName = FName;
         this.LName = LName;
         this.citizenID = citizenID;
+        this.clientType = ClientType;
     }
 
 }
