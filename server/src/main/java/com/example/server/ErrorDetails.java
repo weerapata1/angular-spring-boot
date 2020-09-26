5de785d54bf08f9dbc6144f679b68f4dc0753505
Weerapat T <45629031+weerapata1@users.noreply.github.com>
2020-09-26 23:52:45 +0700

    Update ErrorDetails.java

--- server/src/main/java/com/example/server/ErrorDetails.java
+++ server/src/main/java/com/example/server/ErrorDetails.java
@@ -1,28 +1,21 @@
-//package com.example.server;
-//
-//import java.util.Date;
-//
-//public class ErrorDetails {
-//    private Date timestamp;
-//    private String message;
-//    private String details;
-//
-//    public ErrorDetails(Date timestamp, String message, String details) {
-//        super();
-//        this.timestamp = timestamp;
-//        this.message = message;
-//        this.details = details;
-//    }
-//
-//    public Date getTimestamp() {
-//        return timestamp;
-//    }
-//
-//    public String getMessage() {
-//        return message;
-//    }
-//
-//    public String getDetails() {
-//        return details;
-//    }
-//}
+package com.example.server;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+@Data
+@Getter @Setter
+public class ErrorDetails {
+    private Date timestamp;
+    private String message;
+    private String details;
+
+    public ErrorDetails(Date timestamp, String message, String details) {
+        super();
+        this.timestamp = timestamp;
+        this.message = message;
+        this.details = details;
+    }
+}
